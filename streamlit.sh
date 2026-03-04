1ba01723d105daee3da0aa17bbe78789f06dce46
Filipe Oliveira <filipe.oliveira+araucoit@sonaearauco.com>
2026-03-04 13:37:44 +0000

    refactor: Change working directory to `src/dashboard` before launching the Streamlit app.

--- streamlit.sh
+++ streamlit.sh
@@ -8,5 +8,6 @@ echo "Installing dependencies from requirements.txt..." >&2
 pip install -r /home/site/wwwroot/requirements.txt
 
 echo "Dependencies installed. Launching Streamlit..." >&2
-python -m streamlit run /home/site/wwwroot/src/dashboard/app.py --server.port 8000 --server.address 0.0.0.0
+cd /home/site/wwwroot/src/dashboard
+python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0
 echo "Streamlit launched successfully." >&2
